8cdd3bb7e0b2efbb2761c8457067c992109832b8
David Henzl <david.henzl@plus4u.net>
2022-10-24 19:15:26 +0200

    first commit

---
+++ src/pages/Help.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis modi illo soluta ducimus qui aliquid quibusdam ratione nam vel minima dicta facilis, quaerat doloribus ipsum dolore harum, dolores mollitia officia.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima eius cum dolore iusto quaerat suscipit odit animi perspiciatis, illum nostrum velit molestias id sed natus itaque sit consequatur voluptatum atque.</p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+
+})
+</script>
---
+++ src/pages/Todo.vue
@@ -0,0 +1,94 @@
+<template>
+  <q-page class="bg-grey-3 column">
+    <q-list
+      class="bg-white"
+      separator
+      borded
+    >
+      <q-item
+        v-for="(task, index) in tasks"
+        :key="task.title"
+        @click="task.done = !task.done"
+        :class="{ 'done bg-blue-1' : task.done }"
+        clickable
+        v-ripple
+      >
+        <q-item-section avatar>
+          <q-checkbox
+            v-model="task.done"
+            class="no-pointer-events"
+            color="primary"
+          />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{ task.title }}</q-item-label>
+        </q-item-section>
+        <q-item-section
+          v-if="task.done"
+          side
+        >
+          <q-btn
+            @click.stop="deleteTask(index)"
+            flat
+            round
+            dense
+            color="primary"
+            icon="delete"
+          />
+        </q-item-section>
+      </q-item>
+
+
+    </q-list>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      tasks: [
+        {
+          title: 'Get bananas',
+          done: false
+        },
+        {
+          title: 'Eat bananas',
+          done: false
+        },
+        {
+          title: 'Poo bananas',
+          done: false
+        }
+      ]
+    }
+  },
+  methods: {
+    deleteTask(index) {
+      this.$q.dialog({
+        title: 'Confirm',
+        message: 'Really delete?',
+        cancel: true,
+        persistent: true
+      }).onOk(() => {
+        this.tasks.splice(index, 1)
+        this.$q.notify('Task deleted')
+      })
+
+
+    }
+  }
+
+})
+</script>
+
+<style lang="scss">
+  .done {
+    .q-item__label {
+      text-decoration: line-through;
+      color: #bbb;
+    }
+  }
+</style>
